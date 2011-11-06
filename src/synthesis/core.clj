6ffe353d50544c77381a3dac589a034850023862
Tom Helmuth <trhtom@gmail.com>
2011-11-06 17:38:06 -0500

    Initial coding for query evolving call.

--- src/synthesis/core.clj
+++ src/synthesis/core.clj
@@ -1,1 +1,26 @@
-(ns synthesis.core)
+;; synthesis.core.clj
+;; The main code for query creation.
+;; Tom Hemuth, thelmuth@cs.umass.edu, 2011
+
+(ns synthesis.core
+  (:require [clojure.contrib.sql :as sql]))
+
+;;;;;;;;;;
+;; Query creation.
+
+(pushgp :error-function (fn [program]
+                          (list
+                            (let [embryo-query {:select '()
+                                          :from '()
+                                          :where '()}
+                                  final-state (run-push program (push-item embryo-query :auxiliary (make-push-state)))
+                                  result-query (top-item :auxiliary final-state)]
+                              ;Now, need to create a SFW string, and use it on the database to find the fitness.
+                              ;----for now, just return a random integer
+                              (rand-int 1000))))
+        :atom-generators (list 'string_length
+                               'string_take
+                               'string_concat)
+        :population-size 100
+        :max-generations 50
+        :tournament-size 7)

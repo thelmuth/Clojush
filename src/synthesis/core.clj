347b7565bde78d0d6a275f20758f82c9f0bbdd51
Tom Helmuth <trhtom@gmail.com>
2011-11-09 14:23:05 -0500

    Worked on problem of getting constants from the database columns.

--- src/synthesis/core.clj
+++ src/synthesis/core.clj
@@ -52,10 +52,6 @@
       true nil)))
 
 
-; Off integer stack we get (from top (0) to bottom (n)):
-; 0. col-num
-; 1. comparator number
-; 2. (maybe) a constant to compare with
 
 ;; Here, we could just use a random value (integer indexed) from all possible
 ;; values in column. Then, we wouldn't need anything off of column-type
@@ -66,7 +62,12 @@
 
 ;; Start over with where_constraint.
 ;; This instruction is option 1 above.
-(clojush/define-registered where_constraint
+;-----------------------
+; Off integer stack we get (from top (0) to bottom (n)):
+; 0. col-num
+; 1. comparator number
+; 2. (maybe) a constant to compare with
+(clojush/define-registered where_constraint_from_stack
                            (fn [state]
                              (if (not (>= (count (get state :integer)) 2)) ; We will need at least 2 integers
                                state
@@ -90,6 +91,56 @@
                                                                                               (clojush/pop-item column-type
                                                                                                                 state)))))))))))
 
+
+(defn get-constant-from-column
+  "Gets a constant from column from row index. Optional argument distinct is a boolean that defines whether
+   or not the values in the column should be distinct."
+  ([column index]
+    (get-constant-from-column column index false))
+  ([column index distinct]
+    (let [query (str "SELECT "
+                     (if distinct
+                       "DISTINCT "
+                       "")
+                     (name column)
+                     " FROM adult")
+          results (db/run-db-function db/synthesis-db db/db-query query)]
+      (get (nth results
+                (mod index (count results)))
+           column))))
+
+(get-constant-from-column :education 17 true)
+
+
+; Uses a constant taken from the selected column, where options are not distinct.
+; This makes probability of choosing constant C proportional to the frequency of C in the column.
+(clojush/define-registered where_constraint_from_index
+                           (fn [state]
+                             (if (not (>= (count (get state :integer)) 2)) ; We will need at least 2 integers
+                               state
+                               (let [column (select-column (clojush/stack-ref :integer 0 state))
+                                     column-type (get-column-type column)]
+                                 (if (nil? column-type) ; Check for legit column-type
+                                   state
+                                   (if (or (empty? (get state column-type)) ; Make sure column type isn't empty
+                                           (and (= column-type :integer)
+                                                (not (>= (count (get state :integer)) 3))))
+                                     state
+                                     (let [comparator (nth comparators (mod (clojush/stack-ref :integer 1 state)
+                                                                            (count comparators)))
+                                           constant (if (= column-type :integer)
+                                                      (clojush/stack-ref :integer 2 state)
+                                                      (clojush/stack-ref column-type 0 state))
+                                           constraint (str (name column) " " comparator " " constant)]
+                                       (clojush/push-item constraint :where
+                                                          (clojush/pop-item :integer
+                                                                            (clojush/pop-item :integer
+                                                                                              (clojush/pop-item column-type
+                                                                                                                state)))))))))))
+
+; Uses a constant taken from the selected column, where options are distinct.
+; This makes each distinct option equally likely.
+;(clojush/define-registered where_constraint_distinct_from_index
 
 
 ;;;;;;;;;;
@@ -128,22 +179,26 @@
                 :max-generations 50
                 :tournament-size 7)
 
-;;note: try using db_creation things - see if they work
-#_(db/run-db-function db/synthesis-db db/db-query "SELECT *
-                                                 FROM adult
-                                                 WHERE age < 23 AND education = 'Masters'")
+
 
 ;; Test sfw-map-to-query-string
-(def ex-query
+#_(def ex-query
   (sfw-map-to-query-string {:select ["age" "education" "hours_per_week"]
                             :from ["adult"]
                             :where ["age > 50" 'AND "workclass = \"State-gov\""]}))
 
 #_(db/run-db-function db/synthesis-db db/db-query ex-query)
 
+;; To get an indexed thing from the distinct things
+#_(nth (db/run-db-function db/synthesis-db db/db-query "SELECT DISTINCT education
+                                                           FROM adult
+                                                           WHERE 0=0") 8)
+
+;; To get an indexed thing from non-distinct things
+#_(nth (db/run-db-function db/synthesis-db db/db-query "SELECT education
+                                                           FROM adult
+                                                           WHERE 0=0") 24212)
 
 ;; Test and_constraint
-#_(println (clojush/run-push '(2999 16 71 and_constraint)
-                           (clojush/push-item {:select [] :from [] :where ["age > 50"]}
-                                              :auxiliary
-                                              (clojush/make-push-state))))
+(println (clojush/run-push '(2999 "Masters" 16 3 where_constraint_from_stack)
+                           (clojush/make-push-state)))

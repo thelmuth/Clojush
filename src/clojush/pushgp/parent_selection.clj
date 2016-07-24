6095351ded89756ca492197bb4e16ccb9acfcc4e
Tom Helmuth <helmutht@wlu.edu>
2016-07-24 08:13:32 -0600

    Implemented paired-lexicase selection

--- src/clojush/pushgp/parent_selection.clj
+++ src/clojush/pushgp/parent_selection.clj
@@ -57,6 +57,36 @@
                  (rest cases)))))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; paired lexicase selection
+
+(defn paired-lexicase-selection
+  "Lexicase selection, except considered on pairs (or triples, ..., or n-tuples). First, partition
+   the test cases into pairs. Then, run lexicase, but at each step keep around anyone that is elite
+   on at least one of the tests in the pair (or n-tuple). Thus 1-tuple paired lexicase is equivalent
+   to vanilla lexicase."
+  [pop {:keys [paired-lexicase-tuple-size]}]
+  (loop [survivors (retain-one-individual-per-error-vector pop)
+         case-tuples (partition paired-lexicase-tuple-size
+                               (lshuffle (range (count (:errors (first pop))))))]
+    (println case-tuples)
+    (if (or (empty? case-tuples)
+            (empty? (rest survivors)))
+      (lrand-nth survivors)
+      (let [min-errors-for-cases (map (fn [case]
+                                        (apply min (map #(nth % case)
+                                                        (map #(:errors %) survivors))))
+                                      (first case-tuples))]
+        (recur (filter (fn [ind]
+                         (some identity ; This has the same functionality as (apply or ...), which doesn't work since or is a macro
+                               (map (fn [case min-error-for-case]
+                                      (= (nth (:errors ind) case)
+                                         min-error-for-case))
+                                    (first case-tuples)
+                                    min-errors-for-cases)))
+                       survivors)
+               (rest case-tuples))))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; elitegroup lexicase selection
 
 (defn build-elitegroups

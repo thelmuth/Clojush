c21eeaf9bb0b49b10561e85eb2e3c46470cbdc97
Tom Helmuth <thelmuth@hamilton.edu>
2018-01-22 12:50:32 -0500

    Better formatting for SLT semantics printin

--- src/clojush/pushgp/selection/sampled_lexicase_tournament.clj
+++ src/clojush/pushgp/selection/sampled_lexicase_tournament.clj
@@ -64,8 +64,8 @@
                       (for [[error-vector inds-vector] semantic-maps]
                         (vector error-vector
                                 (apply + (map :lexicase-samples inds-vector))))))
-        (println "Semantic Lexicase Sample Counts for each Error Vector:")
-        (prn @semantic-SLT-samples)))))
+        (println "Semantic Lexicase Sample Counts for each Error Vector:"
+                 (pr-str @semantic-SLT-samples))))))
 
 (defn sampled-lexicase-tournament-selection
   "Uses repeated lexicase selection samples to rank the population by how often

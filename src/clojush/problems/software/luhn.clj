9ee5edd6b1d0e333605ea4154c9cc19da06675d4
Peter Kelly <pxkelly@hamilton.edu>
2019-06-12 16:38:15 -0400

    Implemented Luhn Algorithm

---
+++ src/clojush/problems/software/luhn.clj
@@ -0,0 +1,155 @@
+;; luhn.clj
+;; Peter Kelly, pxkelly@hamilton.edu
+;;
+
+(ns clojush.problems.software.luhn
+  (:use clojush.pushgp.pushgp
+        [clojush pushstate interpreter random util globals]
+        clojush.instructions.tag
+        [clojure.math numeric-tower]
+        ))
+
+; Atom generators
+(def luhn-atom-generators
+  (concat (list
+            ;;; end constants
+            ;;; end ERCs
+            (tag-instruction-erc [:integer :float :boolean :exec] 1000)
+            (tagged-instruction-erc 1000)
+            ;;; end tag ERCs
+            'in1
+            ;;; end input instructions
+            )
+          (registered-for-stacks [:integer :boolean :exec])))
+
+
+;; A list of data domains for the problem. Each domain is a vector containing
+;; a "set" of inputs and two integers representing how many cases from the set
+;; should be used as training and testing cases respectively. Each "set" of
+;; inputs is either a list or a function that, when called, will create a
+;; random element of the set.
+(def luhn-data-domains
+  [[(list 10 91 100000 901) 4 0]
+   [(fn [] (+ (rand-int 99990) 10)) 96 1000]
+   ])
+
+;;Can make Luhn test data like this:
+;(test-and-train-data-from-domains luhn-data-domains)
+
+(defn luhn-test-cases
+  "Takes a sequence of inputs and gives IO test cases of the form
+   [input output]."
+  [inputs]
+  (map (fn [in]
+         (vector in
+           (let [digits (map #(Character/digit % 10) (str in))
+                 split (if (even? (count digits)) (apply map list (partition 2 digits))
+                                                  (apply map list (partition 2 (rest digits))))]
+             (if (= (mod
+                     (reduce +
+                       (flatten (conj (map #(if (> % 9) (- % 9) %)
+                                       (map #(* 2 %)
+                                         (nth split 0)))
+                                      (if (even? (count digits)) (nth split 1)
+                                                                 (conj (nth split 1) (nth digits 0))))))
+                     10) 0) true false))))
+       inputs))
+
+(defn make-luhn-error-function-from-cases
+  [train-cases test-cases]
+  (fn the-actual-luhn-error-function
+    ([individual]
+      (the-actual-luhn-error-function individual :train))
+    ([individual data-cases] ;; data-cases should be :train or :test
+     (the-actual-luhn-error-function individual data-cases false))
+    ([individual data-cases print-outputs]
+      (let [behavior (atom '())
+            errors (doall
+                     (for [[input1 correct-output] (case data-cases
+                                                     :train train-cases
+                                                     :test test-cases
+                                                     [])]
+                       (let [final-state (run-push (:program individual)
+                                                   (->> (make-push-state)
+                                                        (push-item input1 :input)
+                                                        (push-item "" :output)))
+                             result (top-item :boolean final-state)]
+                         (when print-outputs
+                           (println (format "Correct output: %5b | Program output: %s" correct-output (str result))))
+                         ; Record the behavior
+                         (swap! behavior conj result)
+                         ; Error is boolean error
+                         (if (= result correct-output)
+                           0
+                           1))))]
+        (if (= data-cases :train)
+          (assoc individual :behaviors @behavior :errors errors)
+          (assoc individual :test-errors errors))))))
+
+(defn get-luhn-train-and-test
+  "Returns the train and test cases."
+  [data-domains]
+  (map luhn-test-cases
+      (test-and-train-data-from-domains data-domains)))
+
+; Define train and test cases
+(def luhn-train-and-test-cases
+  (get-luhn-train-and-test luhn-data-domains))
+
+(defn luhn-initial-report
+  [argmap]
+  (println "Train and test cases:")
+  (doseq [[i case] (map vector (range) (first luhn-train-and-test-cases))]
+    (println (format "Train Case: %3d | Input/Output: %s" i (str case))))
+  (doseq [[i case] (map vector (range) (second luhn-train-and-test-cases))]
+    (println (format "Test Case: %3d | Input/Output: %s" i (str case))))
+  (println ";;******************************"))
+
+(defn luhn-report
+  "Custom generational report."
+  [best population generation error-function report-simplifications]
+  (let [best-test-errors (:test-errors (error-function best :test))
+        best-total-test-error (apply +' best-test-errors)]
+    (println ";;******************************")
+    (printf ";; -*- Luhn problem report - generation %s\n" generation)(flush)
+    (println "Test total error for best:" best-total-test-error)
+    (println (format "Test mean error for best: %.5f" (double (/ best-total-test-error (count best-test-errors)))))
+    (when (zero? (:total-error best))
+      (doseq [[i error] (map vector
+                             (range)
+                             best-test-errors)]
+        (println (format "Test Case  %3d | Error: %s" i (str error)))))
+    (println ";;------------------------------")
+    (println "Outputs of best individual on training cases:")
+    (error-function best :train true)
+    (println ";;******************************")
+    )) ;; To do validation, could have this function return an altered best individual
+       ;; with total-error > 0 if it had error of zero on train but not on validation
+       ;; set. Would need a third category of data cases, or a defined split of training cases.
+
+
+; Define the argmap
+(def argmap
+  {:error-function (make-luhn-error-function-from-cases (first luhn-train-and-test-cases)
+                                                                   (second luhn-train-and-test-cases))
+   :atom-generators luhn-atom-generators
+   :max-points 2400
+   :max-genome-size-in-initial-program 300
+   :evalpush-limit 15000
+   :population-size 1000
+   :max-generations 300
+   :parent-selection :lexicase
+   :genetic-operator-probabilities {:alternation 0.2
+                                    :uniform-mutation 0.2
+                                    :uniform-close-mutation 0.1
+                                    [:alternation :uniform-mutation] 0.5
+                                    }
+   :alternation-rate 0.01
+   :alignment-deviation 10
+   :uniform-mutation-rate 0.01
+   :problem-specific-report luhn-report
+   :problem-specific-initial-report luhn-initial-report
+   :report-simplifications 0
+   :final-report-simplifications 5000
+   :max-error 1000000
+   })

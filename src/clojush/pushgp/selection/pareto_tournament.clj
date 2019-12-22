1095aa0a429c3ad98190e28273a422e1fad593e0
Tom Helmuth <thelmuth@hamilton.edu>
2019-12-22 17:34:49 -0500

    Changed Pareto-tournament selection to use all individuals on Pareto front as parents.

--- src/clojush/pushgp/selection/pareto_tournament.clj
+++ src/clojush/pushgp/selection/pareto_tournament.clj
@@ -28,21 +28,61 @@
   [ind population objectives]
   (empty? (filter #(dominates? % ind objectives) population)))
 
+(defn distinct-in-objective-space
+  "Returns a sequence of distinct individuals in objective space.
+  So, if two individuals have the same values for each key in
+  objectives, only one with be in the resulting list."
+  [inds objectives]
+  (map rand-nth
+       (vals (group-by (fn [ind] (map #(get ind %) objectives))
+                       inds))))
+
+#_(distinct-in-objective-space
+ '({:a 5 :b 3 :c :A}
+   {:a 2 :b 3 :c :B}
+   {:a 5 :b 3 :c :C}
+   {:a 5 :b 1 :c :D}
+   {:a 5 :b 1 :c :E}
+   {:a 5 :b 1 :c :F}
+   {:a 22 :b 22 :c G})
+ [:a :b])
+
+(defn pareto-tournament-queue-manager
+  "Called on pareto-tournament-queue to return a new parent and manage the queue"
+  [pt-queue population {:keys [tournament-size pareto-tournament-objectives]}]
+  (if (not (empty? (:queue pt-queue)))
+                                        ; If not empty, put first ind in queue as new parent and pop out of queue
+    {:new-parent (first (:queue pt-queue))
+     :queue (rest (:queue pt-queue))}
+                                        ; If empty, find pareto front of a new tournament
+    (let [tournament-set (repeatedly tournament-size #(lrand-nth population))
+          tournament-set-with-size (distinct-in-objective-space
+                                    (if (some #{:size} pareto-tournament-objectives)
+                                      (map (fn [ind]
+                                             (assoc ind :size
+                                                    (count-points (:program ind))))
+                                           tournament-set)
+                                      tournament-set)
+                                    pareto-tournament-objectives)
+          pareto-front (filter #(non-dominated? % tournament-set-with-size pareto-tournament-objectives)
+                               tournament-set-with-size)]
+      (println (map :age pareto-front))
+      
+      {:new-parent (first pareto-front)
+       :queue (rest pareto-front)})))
+
+
+(def pareto-tournament-queue (atom {:new-parent nil
+                                    :queue '()}))
+
 (defn pareto-tournament-selection
-  "Returns a random individual that is on the pareto front of the
-  individuals in the tournament. Objectives are given as
-  pareto-tournament-objectives. Options currently are:
+  "Uses pareto-tournaments to select individuals to be parents.
+  If pareto-tournament-queue is empty, conducts a pareto tournament and
+  fills it. Then, or if it is not empty, the first individual in the queue
+  is selected to be a parent.
+  Objectives are given as  pareto-tournament-objectives. Options currently are:
    :total-error
    :size
    :age"
-  [pop {:keys [tournament-size pareto-tournament-objectives]}]
-  (let [tournament-set (repeatedly tournament-size #(lrand-nth pop))
-        tournament-set-with-size (if (some #{:size} pareto-tournament-objectives)
-                                   (map (fn [ind]
-                                          (assoc ind :size
-                                                 (count-points (:program ind))))
-                                        tournament-set)
-                                   tournament-set)
-        pareto-front (filter #(non-dominated? % tournament-set-with-size pareto-tournament-objectives)
-                             tournament-set-with-size)]
-    (lrand-nth pareto-front)))
+  [population argmap]
+  (:new-parent (swap! pareto-tournament-queue pareto-tournament-queue-manager population argmap)))

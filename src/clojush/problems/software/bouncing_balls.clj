5c14d27a4a60431aa0e2527214bca37303055179
Peter Kelly <pxkelly@hamilton.edu>
2020-07-31 15:15:21 -0700

    Redid Bouncing Balls

--- src/clojush/problems/software/bouncing_balls.clj
+++ src/clojush/problems/software/bouncing_balls.clj
@@ -12,7 +12,7 @@
 ; Atom generators
 (def bouncing-balls-atom-generators
   (concat (list
-            (tag-instruction-erc [:exec :integer :boolean] 1000)
+            (tag-instruction-erc [:exec :integer :boolean :float] 1000)
             (tagged-instruction-erc 1000)
             ;;; end ERCs
             'in1
@@ -22,19 +22,32 @@
             )
           (registered-for-stacks [:integer :boolean :exec :float])))
 
+;; Define test cases
+(defn bouncing-balls-input
+  "Makes a Bouncing Balls input"
+  []
+  (let [start-height (inc (rand 99))
+        bounce-height (inc (* (rand) (dec start-height)))]
+        (vector start-height bounce-height (inc (rand-int 20)))))
+
 ;; A list of data domains for the bouncing-balls problem. Each domain is a vector containing
 ;; a "set" of inputs and two integers representing how many cases from the set
 ;; should be used as training and testing cases respectively. Each "set" of
 ;; inputs is either a list or a function that, when called, will create a
 ;; random element of the set.
 (def bouncing-balls-data-domains
-  [[(list [0 0 0]
-          [100 0.99 50]
-          [3 0.66 1.5]
-          [3 0.5 3]
-          [1 0.3 10]) 5 0]
-   [(fn [] (list (inc (rand 100)) (rand) (inc (rand 50)))) 195 2000]
-   ])
+  [[(list [1.001 1.0 1] ; Smallest input
+          [100.0 99.999 20] ; Largest input
+          [100.0 1.0 20] ; Low bounce index
+          [15.319 5.635 1]
+          [2.176 1.787 1]
+          [17.165 5.627 1]
+          [60.567 37.053 1]
+          [62.145 62.058 1]
+          [36.311 33.399 1]
+          [46.821 8.151 1] ; One bounce
+          ) 10 0]
+   [(fn [] (bouncing-balls-input)) 190 2000]])
 
 ;;Can make bouncing-balls test data like this:
 ;(test-and-train-data-from-domains bouncing-balls-data-domains)
@@ -42,15 +55,14 @@
 ; Helper function for error function
 (defn bouncing-balls-test-cases
   "Takes a sequence of inputs and gives IO test cases of the form
-   [[input1 input2] output]."
+   [[input1 input2 input3] output]."
   [inputs]
   (map (fn [[in1 in2 in3]]
           (vector [in1 in2 in3]
-            (if (<= in1 in3) -1
-              (loop [h in1 times 0]
-                (cond
-                  (<= h in3) (inc times)
-                  :else (recur (* h in2) (inc times)))))))
+            (let [bounce-index (float (/ in2 in1))]
+              (loop [start-height in1 bounce-height in2 bounces-left in3 distance 0]
+                (if (= bounces-left 0) distance
+                  (recur bounce-height (* bounce-height bounce-index) (dec bounces-left) (+ distance start-height bounce-height)))))))
        inputs))
 
 (defn make-bouncing-balls-error-function-from-cases
@@ -63,24 +75,29 @@
     ([individual data-cases print-outputs]
       (let [behavior (atom '())
             errors (doall
-                     (for [[[input1 input2 input3] out-int] (case data-cases
-                                                              :train train-cases
-                                                              :test test-cases
-                                                              [])]
+                     (for [[[input1 input2 input3] correct-output] (case data-cases
+                                                                    :train train-cases
+                                                                    :test test-cases
+                                                                    [])]
                        (let [final-state (run-push (:program individual)
                                                    (->> (make-push-state)
                                                      (push-item input3 :input)
                                                      (push-item input2 :input)
                                                      (push-item input1 :input)))
-                             printed-result (top-item :integer final-state)]
-                         (when print-outputs
-                           (println (format "Correct output: %-19s | Program output: %-19s" (str out-int) printed-result)))
+                             result (top-item :float final-state)]
+                           (when print-outputs
+                             (let [res-str (if (float? result)
+                                             (format "%.3f" result)
+                                             (str result))]
+                               (println (format "Correct output: %.3f | Program output: %s" (float correct-output) res-str))))
                          ; Record the behavior
-                         (swap! behavior conj printed-result)
-                         ; Each test case is integer error
-                         (if (number? printed-result)
-                           (abs (- printed-result out-int)) ;distance from correct integer
-                           1000000))))] ; penalty for no return value
+                         (swap! behavior conj result)
+                         ; Error is float error rounded to 3 decimal places
+                         (round-to-n-decimal-places
+                          (if (number? result)
+                            (abs (- result correct-output)) ; distance from correct integer
+                            1000000.0) ; penalty for no return value
+                          3))))]
         (if (= data-cases :train)
           (assoc individual :behaviors @behavior :errors errors)
           (assoc individual :test-errors errors))))))

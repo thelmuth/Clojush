24bb51aaba4e0d4025ab8580d81db465e5be76e4
Tom Helmuth <thelmuth@hamilton.edu>
2021-02-21 14:57:55 -0500

    Updated downsampled-lexicase to allow for ranges.

--- src/clojush/pushgp/selection/downsampled_lexicase.clj
+++ src/clojush/pushgp/selection/downsampled_lexicase.clj
@@ -3,7 +3,20 @@
 
 (defn down-sample
   "Performs downsampling on training cases by returning only a random subsample
-   of the training cases"
+   of the training cases
+   If downsample-factor is a number, it is used directly.
+   If downsample-factor is a vector [lower upper], then lower and upper are used
+   as lower and upper bounds, and a new factor is uniformly randomly sampled
+   from that range every generation."
   [{:keys [training-cases downsample-factor]}]
-  (take (* downsample-factor (count training-cases))
-        (shuffle training-cases)))
+  (if (number? downsample-factor)
+    (take (* downsample-factor (count training-cases))
+          (shuffle training-cases))
+    (let [lower (first downsample-factor)
+          upper (second downsample-factor)
+          range (- upper lower)
+          ds-factor (+ lower (rand range))]
+      (println "Downsampled lexicase factor this generation:" ds-factor)
+      (take (* ds-factor (count training-cases))
+            (shuffle training-cases)))
+    ))
